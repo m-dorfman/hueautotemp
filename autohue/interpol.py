8d892c923d555fd0e246c68af15e37d4c1dd1837
Mdorfman <supremus-58@users.noreply.github.com>
2016-12-07 20:58:04 -0500

    Update interpol.py

--- autohue/interpol.py
+++ autohue/interpol.py
@@ -1,24 +1,37 @@
 # NOT YET IN USE
 # EARLY INTERPOLATION DEMO
 
-
+from numpy import diff
 from __future__ import division
 
 from numpy import arange, array, linspace, ones, zeros
 from scipy.linalg import solve_banded
 
-import fast_cubic_spline as _spline
+'''
+
+loop this:
+
+    var t = (x - xs[i-1]) / (xs[i] - xs[i-1]);
+		
+    var a =  ks[i-1]*(xs[i]-xs[i-1]) - (ys[i]-ys[i-1]);
+    var b = -ks[i  ]*(xs[i]-xs[i-1]) + (ys[i]-ys[i-1]);
+		
+    var q = (1-t)*ys[i-1] + t*ys[i] + t*(1-t)*(a*(1-t)+b*t);
+    return q;
+'''
 
 def do_plot():
     from matplotlib import pyplot as plt
 
-    line_actual = plt.plot(grid_hat, f(grid_hat), label='actual')
-    line_approx = plt.plot(grid_hat, fhat, '-.', label='interpolated')
+    line_actual = plt.plot(grid_hat, f(grid_hat), label='actual values')
+    line_approx = plt.plot(grid_hat, fhat, '-.', label='interpolated values')
     plt.setp(line_actual, linewidth=1, linestyle='--')
     plt.setp(line_approx, linewidth=2, linestyle='-.')
     plt.legend()
     plt.show()
 
+  
+    
 def coef_do(a, b, y, c=None, alpha=0, beta=0):
     n_val = y.shape[0] - 1
     h = (b - a) / n_val
@@ -32,8 +45,7 @@ def coef_do(a, b, y, c=None, alpha=0, beta=0):
 
     c[1] = 1 / 6 * (y[0] - (alpha * h ** 2) / 6)
     c[n_val + 1] = 1 / 6 * (y[n_val] - (beta * h ** 2) / 6)
-
-    # ab matrix here is just compressed banded matrix
+    
     ab = ones((3, n_val - 1))
     ab[0, 0] = 0
     ab[1, :] = 4
@@ -52,19 +64,13 @@ def coef_do(a, b, y, c=None, alpha=0, beta=0):
     if ifreturn:
         return (c)
 
-
-# aliases
-interpolate = _spline.interpolate
-interpolate_2d = _spline.interpolate_2d
-
 if __name__ == '__main__':
-    #single dim interpol
     f = lambda x: x ** 2
 
     a = -1
     b = 1
-    n_val_ini = 49  # there are n + 1 grid points (0,..., n)
-
+    n_val_ini = 49  # there are n + 1 points over grid
+    
     h = (b - a) / n_val_ini
     grid = arange(n_val_ini + 1) * h + a
 
